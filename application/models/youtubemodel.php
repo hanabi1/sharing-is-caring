084e74281cad1c0c9f03baef5b9454f497d32400
Sebastian.lockwood <sebastian.lockwood@gmail.com>
2014-05-31 16:04:57 +0200

    Added youtube model that checks for a good thumbnail

---
+++ application/models/youtubemodel.php
@@ -0,0 +1,39 @@
+<?php
+
+class YoutubeModel
+{
+    /**
+     * Every model needs a database connection, passed to the model
+     * @param object $db A PDO database connection
+     */
+    function __construct($db) {
+        try {
+            $this->db = $db;
+        } catch (PDOException $e) {
+            exit('Database connection could not be established.');
+        }
+    }
+
+    function getMaximumThumbnailSize($youtubeID=''){
+        if(!$youtubeID){
+            return false;
+        }
+
+        if(get_http_response_code('http://img.youtube.com/vi/' youtubeID '/maxresdefault.jpg') != '404'){
+            return 'maxresdefault.jpg';
+        }elseif(get_http_response_code('http://img.youtube.com/vi/' youtubeID '/hqdefault.jpg') != '404'){
+            return 'hqdefault.jpg';
+        }elseif(get_http_response_code('http://img.youtube.com/vi/' youtubeID '/mqdefault.jpg') != '404'){
+            return 'mqdefault.jpg';
+        }
+        
+        //Worst case scenario we return the default size 
+        return 'default.jpg';
+    }
+    
+    //gets the header response ex 404
+    private function get_http_response_code($url) {
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3);
+    }
+}
