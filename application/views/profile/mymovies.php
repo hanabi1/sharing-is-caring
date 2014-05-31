2795cafc98efc711d9f2751f617b2ab070d3c286
Sebastian.lockwood <sebastian.lockwood@gmail.com>
2014-05-31 17:36:18 +0200

    removed old db export added description into mymovies

--- application/views/profile/mymovies.php
+++ application/views/profile/mymovies.php
@@ -3,11 +3,18 @@
 	    <h2>My Movies</h2>
 	    <?php foreach ($myMovies as $myMovie):?>
 	    	<div class="col-sm-8">
-	    		<?php if(isset($myMovie['youtubeid'],$myMovie['thumbnailres'],$myMovie['title'])):?>
-	    			<a href="http://www.youtube.com/watch?v=<?php echo $myMovie['youtubeid']?>">
-	    				<img src="http://img.youtube.com/vi/<?php echo $myMovie['youtubeid']?>/mqres.jpg" alt="<?php echo $myMovie['title']?>" target="_blank">
-	    			</a>
-	    		<?php endif;?>
+	    		<div class="col-sm-3">
+		    		<?php if(isset($myMovie['youtubeid'],$myMovie['thumbnailres'],$myMovie['title'])):?>
+		    			<a href="http://www.youtube.com/watch?v=<?php echo $myMovie['youtubeid']?>">
+		    				<img src="http://img.youtube.com/vi/<?php echo $myMovie['youtubeid']?>/1.jpg" alt="<?php echo $myMovie['title']?>" target="_blank">
+		    			</a>
+		    		<?php endif;?>
+		    	</div>
+		    	<div class="col-sm-8 col-sm-offset-1">
+		    		<?php if(isset($myMovie['description'])):?>
+		    			<p><?php echo $myMovie['description']?></p>
+		    		<?php endif;?>		    		
+		    	</div>
 	    	</div>	
 	    <?php endforeach ?>
 	</div>
