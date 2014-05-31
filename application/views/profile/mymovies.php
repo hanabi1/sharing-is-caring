7198b56c5b021d1c7a0ca5d40e7ec2967bbc4597
Sebastian.lockwood <sebastian.lockwood@gmail.com>
2014-05-31 17:18:03 +0200

    working on mymovies.....

--- application/views/profile/mymovies.php
+++ application/views/profile/mymovies.php
@@ -5,7 +5,7 @@
 	    	<div class="col-sm-8">
 	    		<?php if(isset($myMovie['youtubeid'],$myMovie['thumbnailres'],$myMovie['title'])):?>
 	    			<a href="http://www.youtube.com/watch?v=<?php echo $videoID?>">
-	    				<img src="http://img.youtube.com/vi/<?php echo $videoID?>/<?php echo $thumbnailres?>" alt="<?php echo $title?>" target="_blank">
+	    				<img src="http://img.youtube.com/vi/<?php echo $myMovie['youtubeid']?>/<?php echo $myMovie['thumbnailres']?>" alt="<?php echo $myMovie['title']?>" target="_blank">
 	    			</a>
 	    		<?php endif;?>
 	    	</div>	
