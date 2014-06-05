8448ba533f9d603fc06a9e9bded2146da8c75b86
Sebastian.lockwood <sebastian.lockwood@gmail.com>
2014-06-05 15:10:47 +0200

    Added links in footer

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -2,7 +2,9 @@
 			</div><!--End the page row started in header.php-->
 		</div> <!--This container started in header.php-->
 		<footer>
-			<small>| Sharing Is Caring&copy; | Multi-corp International&reg; |</small>
+			<small>Sharing is Caring &copy;2014</small>
+			<br>
+			<small><a href="#" alt="Kim Ferm">Kim Ferm</a> | <a href="//www.sebastianlockwood.com" alt="Sebastian Lockwood">Sebastian Lockwood</a></small></small>
 		</footer>
 	</body>
 </html>
