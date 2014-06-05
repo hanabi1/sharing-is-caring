dde07471abb651bcd7bb8cd305c138b57b5ed34a
Sebastian.lockwood <sebastian.lockwood@gmail.com>
2014-06-05 14:53:24 +0200

    Fixed search bar buggs

--- application/views/movies/search.php
+++ application/views/movies/search.php
@@ -1,4 +1,4 @@
-<div class="col-sm-8 col-sm-offset-1">
+<div class="col-sm-7 col-sm-offset-1">
     <h2>Search</h2>
     <h3>Found: <?php echo count($myMovies)?> <?php if(count($myMovies)==1) echo 'match'; else echo 'matches';?></h3>
     <?php foreach ($myMovies as $myMovie):?>
@@ -6,7 +6,7 @@
     		<div class="col-sm-3">
 	    		<?php if(isset($myMovie['youtubeid'],$myMovie['thumbnailres'],$myMovie['title'],$myMovie['link'])):?>
 	    			<a href="http://www.youtube.com/watch?v=<?php echo $myMovie['youtubeid']?>">
-	    				<img class="video-thumbnail" src="<?php echo $myMovie['link']?>" title="<?php echo $myMovie['title']?>" alt="<?php echo $myMovie['title']?>" target="_blank">
+	    				<img class="video-thumbnail" src="<?php echo $myMovie['thumbnailres']?>" title="<?php echo $myMovie['title']?>" alt="<?php echo $myMovie['title']?>" target="_blank">
 	    			</a>
 	    		<?php endif;?>
 	    	</div>
